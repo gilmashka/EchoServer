2e5cbc1ed4c008c760ebc763508d8a5b0066386a
gilmashka <loxozavr33@gmail.com>
2026-04-17 13:56:00 +0300

    обновлена безопасность через использование Principal

--- src/main/java/ru/technocracy/echoserver/controllers/CategoryController.java
+++ src/main/java/ru/technocracy/echoserver/controllers/CategoryController.java
@@ -4,8 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.technocracy.echoserver.dto.UserCategoryPreferenceDto;
+import ru.technocracy.echoserver.models.User;
 import ru.technocracy.echoserver.services.CategoryService;
+import ru.technocracy.echoserver.services.UserService;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -15,19 +18,24 @@ import java.util.List;
 public class CategoryController {
 
     private final CategoryService categoryService;
+    private final UserService userService;
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<List<UserCategoryPreferenceDto>> getAll(@PathVariable Long userId){
-        List<UserCategoryPreferenceDto> categories =  categoryService.getAllCategoriesWithPreference(userId);
+    @GetMapping()
+    public ResponseEntity<List<UserCategoryPreferenceDto>> getAll(Principal principal){
+        User user = userService.getUserByNickname(principal.getName());
+
+        List<UserCategoryPreferenceDto> categories =  categoryService.getAllCategoriesWithPreference(user.getId());
         return ResponseEntity.status(200).body(categories);
     }
 
-    @PostMapping("/{userId}/preference")
+    @PostMapping("/preference")
     public ResponseEntity<Void> setUserPreference(
-            @PathVariable Long userId,
+            Principal principal,
             @RequestBody UserCategoryPreferenceDto dto) {
 
-        categoryService.setUserPreference(dto, userId);
+        User user = userService.getUserByNickname(principal.getName());
+
+        categoryService.setUserPreference(dto, user.getId());
         return ResponseEntity.status(201).build();
     }
 }
--- src/main/java/ru/technocracy/echoserver/controllers/FeedController.java
+++ src/main/java/ru/technocracy/echoserver/controllers/FeedController.java
@@ -4,9 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.technocracy.echoserver.exceptions.NotFoundException;
+import ru.technocracy.echoserver.models.User;
 import ru.technocracy.echoserver.services.EventService;
 import ru.technocracy.echoserver.services.FeedService;
+import ru.technocracy.echoserver.services.UserService;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -17,20 +20,25 @@ public class FeedController {
 
     private final FeedService feedService;
     private final EventService eventService;
+    private final UserService userService;
 
-    @GetMapping("/favourite/{userId}")
-    public ResponseEntity<?> getFavouriteFeed(@PathVariable Long userId){
+    @GetMapping("/favourite")
+    public ResponseEntity<?> getFavouriteFeed(Principal principal){
+        User user = userService.getUserByNickname(principal.getName());
+
         try {
-            return ResponseEntity.status(200).body(feedService.getUserFavoritesEventFeed(userId));
+            return ResponseEntity.status(200).body(feedService.getUserFavoritesEventFeed(user.getId()));
         } catch (NotFoundException e){
             return ResponseEntity.status(404).body(e.getMessage());
         }
     }
 
-    @GetMapping("/neutral/{userId}")
-    public ResponseEntity<?> getNeutralFeed(@PathVariable Long userId){
+    @GetMapping("/neutral")
+    public ResponseEntity<?> getNeutralFeed(Principal principal){
+        User user = userService.getUserByNickname(principal.getName());
+
         try {
-            return ResponseEntity.status(200).body(feedService.getUserNeutralEventFeed(userId));
+            return ResponseEntity.status(200).body(feedService.getUserNeutralEventFeed(user.getId()));
         } catch (NotFoundException e){
             return ResponseEntity.status(404).body(e.getMessage());
         }
